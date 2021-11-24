571060557e62e17c48c4277a72437b472feda3f1
Svetlana Vassileva <svetlilaloli@gmail.com>
2021-11-24 10:46:09 +0000

    ExportAlbumsInfo method extended

--- MusicHub/MusicHub/StartUp.cs
+++ MusicHub/MusicHub/StartUp.cs
@@ -1,7 +1,8 @@
 ﻿namespace MusicHub
 {
     using System;
-
+    using System.Linq;
+    using System.Text;
     using Data;
     using Initializer;
 
@@ -15,11 +16,35 @@
             DbInitializer.ResetDatabase(context);
 
             //Test your solutions here
+            //Console.WriteLine(ExportAlbumsInfo(context, 9));
         }
 
         public static string ExportAlbumsInfo(MusicHubDbContext context, int producerId)
         {
-            throw new NotImplementedException();
+            StringBuilder result = new StringBuilder();
+
+            var albums = context.Albums.Where(a => a.ProducerId == producerId).ToList();
+
+            foreach (var album in albums.OrderByDescending(a => a.Price))
+            {
+                result.AppendLine($"-AlbumName: {album.Name}");
+                result.AppendLine($"-ReleaseDate: {album.ReleaseDate:MM/dd/yyyy}");
+                result.AppendLine($"-ProducerName: {album.Producer.Name}");
+                result.AppendLine($"-Songs:");
+                
+                int songNum = 1;
+                foreach (var song in album.Songs.OrderByDescending(s => s.Name).ThenBy(s => s.Writer.Name))
+                {
+                    result.AppendLine($"---#{songNum}");
+                    result.AppendLine($"---SongName: {song.Name}");
+                    result.AppendLine($"---Price: {song.Price:f2}");
+                    result.AppendLine($"---Writer: {song.Writer.Name}");
+                    songNum++;
+                }
+                result.AppendLine($"-AlbumPrice: {album.Price:f2}");
+            }
+
+            return result.ToString().TrimEnd();
         }
 
         public static string ExportSongsAboveDuration(MusicHubDbContext context, int duration)
