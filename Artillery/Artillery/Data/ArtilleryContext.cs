c724bd8b040cdfa839989fe50199c9c882731087
Svetlana Vassileva <svetlilaloli@gmail.com>
2022-01-20 06:19:30 +0000

    Skeleton

---
+++ Artillery/Artillery/Data/ArtilleryContext.cs
@@ -0,0 +1,26 @@
+﻿namespace Artillery.Data
+{
+    using Microsoft.EntityFrameworkCore;
+
+    public class ArtilleryContext : DbContext
+    {
+        public ArtilleryContext() { }
+
+        public ArtilleryContext(DbContextOptions options)
+            : base(options) { }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                optionsBuilder
+                    .UseSqlServer(Configuration.ConnectionString);
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+
+        }
+    }
+}
