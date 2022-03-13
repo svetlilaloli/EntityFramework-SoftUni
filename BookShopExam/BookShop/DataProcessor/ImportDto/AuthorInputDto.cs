074452f015e49819b5cc13a6c10ff3ddfa59516b
svetlilaloli <svetlilaloli@gmail.com>
2022-03-13 17:24:28 +0000

    Input Dtos created

---
+++ BookShopExam/BookShop/DataProcessor/ImportDto/AuthorInputDto.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BookShop.DataProcessor.ImportDto
+{
+    public class AuthorInputDto
+    {
+        [Required, MinLength(Constants.NameMinLength), MaxLength(Constants.NameMaxLength)]
+        public string FirstName { get; set; }
+        [Required, MinLength(Constants.NameMinLength), MaxLength(Constants.NameMaxLength)]
+        public string LastName { get; set; }
+        [Required, RegularExpression(Constants.PhonePattern)]
+        public string Phone { get; set; }
+        [Required, RegularExpression(Constants.EmailPattern)]
+        public string Email { get; set; }
+        public BookIdInputDto[] Books { get; set; }
+    }
+}
