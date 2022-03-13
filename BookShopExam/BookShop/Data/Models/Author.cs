d7fbc907a2d5ab49c951fac2f18e2b237bd01150
svetlilaloli <svetlilaloli@gmail.com>
2022-03-13 17:20:27 +0000

    Models created

--- BookShopExam/BookShop/Data/Models/Author.cs
+++ BookShopExam/BookShop/Data/Models/Author.cs
@@ -15,10 +15,10 @@ namespace BookShop.Data.Models
         public string FirstName { get; set; }
         [Required, MinLength(Constants.NameMinLength), MaxLength(Constants.NameMaxLength)]
         public string LastName { get; set; }
-        [Required, RegularExpression(Constants.EmailPattern)]
-        public string Email { get; set; }
         [Required, RegularExpression(Constants.PhonePattern)]
         public string Phone { get; set; }
+        [Required, RegularExpression(Constants.EmailPattern)]
+        public string Email { get; set; }
         public virtual ICollection<AuthorBook> AuthorsBooks { get; set; }
     }
 }
