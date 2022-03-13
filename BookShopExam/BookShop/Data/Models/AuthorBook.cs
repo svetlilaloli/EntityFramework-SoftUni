5b6be1925d0de88dadab157d61257a24c9a9c48f
svetlilaloli <svetlilaloli@gmail.com>
2022-03-13 17:21:24 +0000

    Models created

--- BookShopExam/BookShop/Data/Models/AuthorBook.cs
+++ BookShopExam/BookShop/Data/Models/AuthorBook.cs
@@ -1,13 +1,14 @@
-﻿using System.ComponentModel.DataAnnotations.Schema;
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace BookShop.Data.Models
 {
     public class AuthorBook
     {
-        [ForeignKey(nameof(Author))]
+        [ForeignKey(nameof(Author)), Required]
         public int AuthorId { get; set; }
         public virtual Author Author { get; set; }
-        [ForeignKey(nameof(Book))]
+        [ForeignKey(nameof(Book)), Required]
         public int BookId { get; set; }
         public virtual Book Book { get; set; }
     }
